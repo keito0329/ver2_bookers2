a790579905acc0ea43cfac8a4462f335d75f1f52
keito0329 <172224494+keito0329@users.noreply.github.com>
2024-12-30 06:02:00 +0000

    [Add] User Profile Image

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'homes/about', as: 'about'
-  root to: 'homes#top'
+  
   devise_for :users
+  root to: 'homes#top'
+
+  get 'homes/about', as: 'about'
+
+  resources :books, only: [:index, :show, :edit, :update, :destroy, :create, :new]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
