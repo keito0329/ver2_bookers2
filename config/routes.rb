96a47d06d3e0788422e89dc748301cd829fd29f0
keito0329 <172224494+keito0329@users.noreply.github.com>
2025-01-03 14:12:05 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
 
   get 'home/about' => 'homes#top', as: 'about'
 
-  resources :books, only: [:index, :show, :edit, :update, :destroy, :create, :new]
+  resources :books, only: [:index, :show, :edit, :update, :destroy, :create, :new] do
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update, :index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
